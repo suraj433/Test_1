c6d4ef0cfb2b09f97423dd5f4b263894da2d2eab
user.name : "surajmba9900@gmail.com <surajmba9900@gmail.com>
2026-04-20 10:34:59 +0530

    suraj_5

--- a.py
+++ a.py
@@ -6,3 +6,5 @@ for i in range(0,11):
     print("suraj") 
     print()
 print("This is a simple Python program.")
+print("We are learning how to write Python code.")
+print("Python is a versatile programming language.")
