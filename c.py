a546f26e22f165a9b2a6ba32b4541e76d8a97f7e
user.name : "surajmba9900@gmail.com <surajmba9900@gmail.com>
2026-04-30 10:35:28 +0530

    commit_4

---
+++ c.py
@@ -0,0 +1,14 @@
+print("Hello, World!")
+a = 5
+b = 3
+print("Sum is:", a + b)
+
+num = 10
+if num > 5:
+    print("Number is greater than 5")
+else:
+    print("Number is 5 or less")
+
+fruits = ["apple", "banana", "mango"]
+for fruit in fruits:
+    print(fruit)
